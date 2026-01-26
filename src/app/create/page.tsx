e64984ba26a4b68f2ae1b04807d9012fea527a34
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-27 01:10:40 +0200

    feat:add Create component to render CreatePostPage

---
+++ src/app/create/page.tsx
@@ -0,0 +1,11 @@
+
+import CreatePostPage from "@/components/CreatePostPage";
+
+export default async function Create() {
+    return (
+    <main className="max-w-[935px] mx-auto px-4 pt-8 pb-20">
+        <div className="flex flex-col items-center">
+            <CreatePostPage/>
+        </div>
+    </main>
+)}
