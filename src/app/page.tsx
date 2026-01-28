d72cdb79f943f604c11f0122929ace52c0741c74
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-28 15:39:37 +0200

    feat: add session validation and redirect to login for unauthorized access

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,18 @@ import PostCard from "@/components/PostCard";
 
 import {Header} from "@/components/Header";
 import {getPosts} from "@/lib/db";
+import {getAuthSession} from "@/lib/auth";
+import {redirect} from "next/navigation";
 export const dynamic = 'force-dynamic'
 
 export default async function Page() {
     const posts = await getPosts()
+
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+        redirect("/login");
+    }
 
 
     return (
@@ -15,7 +23,7 @@ export default async function Page() {
             <main className="max-w-[935px] mx-auto px-4 pt-8 pb-20">
                 <div className="flex flex-col items-center">
                     {posts.map(post => (
-                        <PostCard key={post.id} post={post} />
+                        <PostCard key={post.id} post={post}/>
                     ))}
                 </div>
             </main>
